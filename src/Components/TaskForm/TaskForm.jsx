0813eef6e1f1d068f44df89dc8d9cc651552f62c
roksana-barna <roksanabarna1803@gmail.com>
2024-02-12 14:34:22 +0600

    task form

--- src/Components/TaskForm/TaskForm.jsx
+++ src/Components/TaskForm/TaskForm.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const TaskForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const TaskForm = ({ task, onChange, onSave, onCancel }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        name="text"
+        value={task.text}
+        onChange={onChange}
+        placeholder="Task name"
+        className="border p-1 mr-2"
+      />
+      {/* Other form fields for priority, etc. */}
+      <button onClick={onSave} className="mr-2">
+        Save
+      </button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  );
 };
 
 export default TaskForm;
+
+
+
+
+
 
