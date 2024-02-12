689d8ac081fc4a5dc09bf522381acb2068dbee2b
roksana-barna <roksanabarna1803@gmail.com>
2024-02-12 14:24:07 +0600

    task list

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
 
-const TaskList = () => {
-    return (
-        <div>
-            
+import React from "react";
+import TaskItem from "../TaskItem/TaskItem";
+
+const TaskList = ({ tasks, editTask, toggleTaskStatus, deleteTask }) => {
+  return (
+    <div className="mt-4">
+      {tasks.map((task) => (
+        <div key={task.id} className="mb-2 p-2 border rounded">
+          <TaskItem
+            task={task}
+            editTask={editTask}
+            toggleTaskStatus={toggleTaskStatus}
+            deleteTask={deleteTask}
+          />
+          <p>Task Name: {task.text}</p>
+          <p>Priority: {task.priority}</p>
+          <p>Status: {task.completed ? "Completed" : "Incomplete"}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default TaskList;
